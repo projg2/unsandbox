54891d6b76244a72f016d29f88c53ddd80dfa8f9
Michał Górny <mgorny@gentoo.org>
2017-02-15 18:46:29 +0100

    Import v1

---
+++ unsandbox.c
@@ -0,0 +1,35 @@
+/* Escape the Gentoo sandbox. Needed for ASAN tests to work. */
+/* (c) 2016 Michał Górny, 2-clause BSD license */
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+	pid_t pid;
+	int exitst;
+	void* libc;
+	int (*execvp_func)(const char *, char *const[]);
+
+	assert(argv[1]);
+
+	unsetenv("LD_PRELOAD");
+	libc = dlopen("libc.so.6", RTLD_NOW);
+	if (!libc)
+	{
+		perror("dlopen() failed");
+		return EXIT_FAILURE;
+	}
+	execvp_func = dlsym(libc, "execvp");
+	assert(execvp_func);
+
+	execvp_func(argv[1], &argv[1]);
+	perror("execvp() failed");
+
+	dlclose(libc);
+	return EXIT_FAILURE;
+}
